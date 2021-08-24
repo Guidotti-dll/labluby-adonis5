243e30afd3fa04bfb07e0bb6ba7948c79a9d31bd
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-08-24 15:19:50 -0300

    feat: add Exceptions

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -1,23 +1,23 @@
-/*
-|--------------------------------------------------------------------------
-| Http Exception Handler
-|--------------------------------------------------------------------------
-|
-| AdonisJs will forward all exceptions occurred during an HTTP request to
-| the following class. You can learn more about exception handling by
-| reading docs.
-|
-| The exception handler extends a base `HttpExceptionHandler` which is not
-| mandatory, however it can do lot of heavy lifting to handle the errors
-| properly.
-|
-*/
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
+  protected statusPages = {
+    '404': 'errors/not-found',
+    '500..599': 'errors/server-error',
+  }
+
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error: any, ctx: HttpContextContract) {
+    if (error.code === 'E_UNAUTHORIZED_ACCESS') {
+      return ctx.response.status(401).send({ message: "You don't have permission to access " })
+    }
+
+    return super.handle(error, ctx)
   }
 }
