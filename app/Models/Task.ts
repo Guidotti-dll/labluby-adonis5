b7ff785ac76c4d9017f929e405c709b1468143ba
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-08-24 17:43:50 -0300

    feat: send email newTask

--- app/Models/Task.ts
+++ app/Models/Task.ts
@@ -1,8 +1,10 @@
 import { DateTime } from 'luxon'
-import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { afterSave, BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
 import Project from './Project'
 import File from './File'
+import Mail from '@ioc:Adonis/Addons/Mail'
+import Application from '@ioc:Adonis/Core/Application'
 
 export default class Task extends BaseModel {
   @column({ isPrimary: true })
@@ -44,4 +46,27 @@ export default class Task extends BaseModel {
     foreignKey: 'file_id',
   })
   public file: BelongsTo<typeof File>
+
+  @afterSave()
+  public static async send(task: Task) {
+    const user = await User.findBy('id', task.user_id)
+    const file = await File.findBy('id', task.file_id)
+
+    await Mail.sendLater((message) => {
+      message
+        .from('contato@email.com')
+        .to(user!.email)
+        .subject('New Task')
+        .htmlView('emails/new_task', {
+          username: user!.username,
+          title: task.title,
+          hasAttachment: !!task.file_id,
+        })
+      if (file) {
+        message.attach(Application.tmpPath(`uploads/${file!.file}`), {
+          filename: file!.name,
+        })
+      }
+    })
+  }
 }
